7e983c33e23c9aa4066bb114424f447a3fe2da77
aditya85456 <86275783+aditya85456@users.noreply.github.com>
2021-06-22 10:14:02 +0530

    Update and rename sample_config.py to config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -2,13 +2,14 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1668184294:AAENZFr4PNJ5lfPizRgcKb_nX2yVz69fKT0")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", 5554364))
+    API_HASH = os.environ.get("API_HASH", 35e7dd342cbf2c63bf5d9b9ccf8440c5)
+    OWNER_ID = int(os.environ.get("OWNER_ID",1875387358))
     # Get these values from my.telegram.org
     # to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "").split())
+    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "-1001375564520").split())
     # For Admin Commands add user IDs 
     SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
     # the download location, where the HTTP Server runs
